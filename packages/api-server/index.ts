d7cbf35cbb1aa81811c1f3ca5574262c1e100ece
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-04 16:21:49 -0600

    added mutation

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import * as trpc from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import cors from "cors";
-import z from "zod";
+import { z } from "zod";
 
 interface ChatMessage {
   user: string;
@@ -22,8 +22,20 @@ const appRouter = trpc
     },
   })
   .query("getMessages", {
-    resolve() {
-      return messages;
+    input: z.number().default(10),
+    resolve({ input }) {
+      return messages.slice(-input);
+    },
+  })
+  .mutation("addMessage", {
+    input: z.object({
+      user: z.string(),
+      message: z.string(),
+    }),
+    resolve({ input }) {
+      messages.push(input);
+
+      return input;
     },
   });
 
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,11 +8,18 @@ import { trpc } from "./trpc";
 const client = new QueryClient();
 
 const AppContent = () => {
-  const hello = trpc.useQuery(["hello"]);
-  console.log(trpc.useQuery(["hello"]));
+  const getMessages = trpc.useQuery(["getMessages"]);
+  const addMessage = trpc.useMutation("addMessage");
+
+  const onAdd = () =>
+    addMessage.mutate({
+      message: "Hello",
+      user: "Alex",
+    });
+
   return (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      <div>Hello From: {JSON.stringify(hello.data)}</div>
+      <div>Hello From: {JSON.stringify(getMessages.data)}</div>
     </div>
   );
 };
